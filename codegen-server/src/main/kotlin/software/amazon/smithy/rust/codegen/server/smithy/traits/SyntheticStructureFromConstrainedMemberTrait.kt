774f89c97afad7b4c3efeea212b88763785104bd
Fahad Zubair <fahadzub@amazon.com>
2023-01-31 11:22:26 +0000

    Map member shape has been added

--- codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/traits/SyntheticStructureFromConstrainedMemberTrait.kt
+++ codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/traits/SyntheticStructureFromConstrainedMemberTrait.kt
@@ -1,54 +1,14 @@
 package software.amazon.smithy.rust.codegen.server.smithy.traits
 
-import software.amazon.smithy.model.SourceLocation
 import software.amazon.smithy.model.node.Node
-import software.amazon.smithy.model.node.ObjectNode
 import software.amazon.smithy.model.shapes.ShapeId
-import software.amazon.smithy.model.traits.AbstractTrait
-import software.amazon.smithy.model.traits.AbstractTraitBuilder
 import software.amazon.smithy.model.traits.AnnotationTrait
-import software.amazon.smithy.model.traits.PropertyTrait
-import software.amazon.smithy.utils.ToSmithyBuilder
-import java.util.*
 
 /**
  * Trait applied to a refactored shape indicating the structure that contains member of this new structure type
  */
-class RefactoredStructureTrait(val sourceMember : ShapeId): AnnotationTrait(RefactoredStructureTrait.ID, Node.objectNode())  {
+class SyntheticStructureFromConstrainedMemberTrait(val sourceMember : ShapeId): AnnotationTrait(SyntheticStructureFromConstrainedMemberTrait.ID, Node.objectNode())  {
     companion object {
         val ID : ShapeId = ShapeId.from("smithy.api.internal#refactoredMember")
     }
 }
-
-//class RefactoredStructureTrait(private val sourceMember : ShapeId) : AbstractTrait(RefactoredStructureTrait.ID, Node.objectNode()),
-//    ToSmithyBuilder<RefactoredStructureTrait> {
-//    companion object
-//    {
-//        val ID : ShapeId = ShapeId.from("smithy.api.internal#refactoredMember")
-//    }
-//
-//    override  fun isSynthetic(): Boolean {
-//        return true
-//    }
-//    override fun createNode(): Node {
-//        val map = mutableMapOf<String, Node>(
-//            "originalSource" to Node.from(sourceMember.toString()),
-//        )
-//        return ObjectNode(map.mapKeys { Node.from(it.key) }, SourceLocation.NONE)
-//    }
-//
-//    fun builder(): Builder {
-//        return Builder(sourceMember)
-//    }
-//
-//    override fun toBuilder(): Builder? {
-//        return builder().sourceLocation(sourceLocation)!!
-//    }
-//
-//    class Builder(val sourceMember: ShapeId) :
-//        AbstractTraitBuilder<PropertyTrait?, Builder?>() {
-//        override fun build(): PropertyTrait {
-//            return PropertyTrait(this)
-//        }
-//    }
-//}
--- codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/transformers/ConstrainedMemberTransform.kt
+++ codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/transformers/ConstrainedMemberTransform.kt
@@ -5,46 +5,61 @@ import software.amazon.smithy.model.shapes.AbstractShapeBuilder
 import software.amazon.smithy.model.shapes.MemberShape
 import software.amazon.smithy.model.shapes.Shape
 import software.amazon.smithy.model.shapes.ShapeId
-import software.amazon.smithy.model.shapes.StructureShape
-import software.amazon.smithy.model.traits.EnumTrait
-import software.amazon.smithy.model.traits.LengthTrait
-import software.amazon.smithy.model.traits.PatternTrait
-import software.amazon.smithy.model.traits.RangeTrait
+import software.amazon.smithy.model.traits.RequiredTrait
 import software.amazon.smithy.model.traits.Trait
-import software.amazon.smithy.model.traits.UniqueItemsTrait
 import software.amazon.smithy.model.transform.ModelTransformer
 import software.amazon.smithy.rust.codegen.core.smithy.DirectedWalker
-import software.amazon.smithy.rust.codegen.core.util.orNull
-import software.amazon.smithy.rust.codegen.server.smithy.traits.RefactoredStructureTrait
+import software.amazon.smithy.rust.codegen.server.smithy.traits.SyntheticStructureFromConstrainedMemberTrait
 import software.amazon.smithy.utils.ToSmithyBuilder
 import java.lang.IllegalStateException
 import java.util.*
+import software.amazon.smithy.rust.codegen.core.util.UNREACHABLE
+import software.amazon.smithy.rust.codegen.core.util.orNull
+import software.amazon.smithy.rust.codegen.server.smithy.allConstraintTraits
 
-object ConstrainedStructureMemberTransform {
-    private data class MemberTransformation(
+/**
+ * Transforms all member shapes that have constraints on them into equivalent non-constrained
+ * member shapes targeting synthetic constrained structure shapes with the member's constraints.
+ *
+ * E.g.:
+ *
+ * structure A {
+ *   @length(min: 1, max: 69)
+ *   string: ConstrainedString
+ * }
+ *
+ * @length(min: 2, max: 10)
+ * @pattern("^[A-Za-z]+$")
+ * string ConstrainedString
+ *
+ * to
+ *
+ * structure A {
+ *   string: OverriddenConstrainedString
+ * }
+ *
+ * @length(min: 1, max: 69)
+ * @pattern("^[A-Za-z]+$")
+ * OverriddenConstrainedString
+ *
+ * @length(min: 2, max: 10)
+ * @pattern("^[A-Za-z]+$")
+ * string ConstrainedString
+ */
+object ConstrainedMemberTransform {
+    private data class MemberShapeTransformation(
         val newShape: Shape,
         val memberToChange: MemberShape,
         val traitsToKeep: List<Trait>,
     )
 
-    fun Shape.hasMemberConstraintTrait() =
+    private val memberConstraintTraitsToOverride = allConstraintTraits - RequiredTrait::class.java
+
+    private fun Shape.hasMemberConstraintTrait() =
         memberConstraintTraitsToOverride.any(this::hasTrait)
 
-    private val memberConstraintTraitsToOverride = setOf(
-        LengthTrait::class.java,
-        PatternTrait::class.java,
-        RangeTrait::class.java,
-        UniqueItemsTrait::class.java,
-        EnumTrait::class.java,
-    )
-
-    /**
-     * Transforms all member shapes that have constraints on them into equivalent
-     * non-constrained member shapes.
-     */
     fun transform(model: Model): Model {
-        val existingRefactoredNames = HashSet<ShapeId>()
-        val walker = DirectedWalker(model)
+        val additionalNames = HashSet<ShapeId>()
 
         val transformations = model.operationShapes
             .flatMap { operation ->
@@ -52,23 +67,30 @@ object ConstrainedStructureMemberTransform {
             }
             .map { model.expectShape(it) }
             .flatMap {
+                val walker = DirectedWalker(model)
                 walker.walkShapes(it)
             }
-            .filter{ it.isStructureShape || it.isListShape || it.isUnionShape }
+            .filter { it.isStructureShape || it.isListShape || it.isUnionShape || it.isMapShape }
             .flatMap {
-                it.constraintMembers()
-            }.mapNotNull { it.makeNonConstrained(model, existingRefactoredNames) }
+                it.constrainedMembers()
+            }
+            .mapNotNull {
+                val transformation = it.makeNonConstrained(model, additionalNames)
+                if (transformation != null)
+                    additionalNames.add(transformation.newShape.id)
+
+                transformation
+            }
 
         return applyTransformations(model, transformations)
     }
 
     /***
-     * Returns a Model that has all the transformations applied on the original
-     * model.
+     * Returns a Model that has all the transformations applied on the original model.
      */
     private fun applyTransformations(
         model: Model,
-        transformations: List<MemberTransformation>,
+        transformations: List<MemberShapeTransformation>,
     ): Model {
         if (transformations.isEmpty())
             return model
@@ -95,17 +117,17 @@ object ConstrainedStructureMemberTransform {
     /**
      * Returns a list of members that have constraint traits applied to them
      */
-    private fun Shape.constraintMembers(): List<MemberShape> =
+    private fun Shape.constrainedMembers(): List<MemberShape> =
         this.allMembers.values.filter {
             it.hasMemberConstraintTrait()
         }
 
     /**
-     * Returns the unique (within the model) name of the refactored shape
+     * Returns the unique (within the model) name of the new shape
      */
-    private fun refactoredShapeId(
+    private fun overriddenShapeId(
         model: Model,
-        existingRefactoredNames: MutableSet<ShapeId>,
+        additionalNames: Set<ShapeId>,
         memberShape: ShapeId,
     ): ShapeId {
         val structName = memberShape.name
@@ -113,43 +135,24 @@ object ConstrainedStructureMemberTransform {
             .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 
         fun makeStructName(suffix: String = "") =
-            ShapeId.from("${memberShape.namespace}#Refactored${structName}${memberName}$suffix")
+            ShapeId.from("${memberShape.namespace}#Overridden${structName}${memberName}$suffix")
 
         fun structNameIsUnique(newName: ShapeId) =
-            model.getShape(newName).isEmpty && !existingRefactoredNames.contains(newName)
+            model.getShape(newName).isEmpty && !additionalNames.contains(newName)
 
         fun generateUniqueName(): ShapeId {
             // Ensure the name does not already exist in the model, else make it unique
             // by appending a new number as the suffix.
-            var suffix = ""
             (0..100).forEach {
-                val extractedStructName = makeStructName(suffix)
+                val extractedStructName = if (it == 0) makeStructName("") else makeStructName("$it")
                 if (structNameIsUnique(extractedStructName))
                     return extractedStructName
-
-                suffix = "_$it"
             }
 
-            // A unique type could not be found by adding a numeric suffix to it. As a
-            // secondary algorithm, use existing struct names as suffix to generate a unique name.
-            suffix = "_"
-
-            model.listShapes
-                .filterIsInstance<StructureShape>()
-                .map {
-                    suffix = suffix.plus(it.id.name)
-
-                    val extractedStructName = makeStructName(suffix)
-                    if (structNameIsUnique(extractedStructName))
-                        return extractedStructName
-                }
-
-            throw IllegalStateException("A unique name for the refactored structure type could not be generated")
+            throw IllegalStateException("A unique name for the overridden structure type could not be generated")
         }
 
-        val newName = generateUniqueName()
-        existingRefactoredNames.add(newName)
-        return newName
+        return generateUniqueName()
     }
 
     /**
@@ -158,50 +161,48 @@ object ConstrainedStructureMemberTransform {
      */
     private fun MemberShape.makeNonConstrained(
         model: Model,
-        existingRefactoredNames: MutableSet<ShapeId>,
-    ): MemberTransformation? {
-        val (constrainedTraits, otherTraits) = this.allTraits.values
+        additionalNames: MutableSet<ShapeId>,
+    ): MemberShapeTransformation? {
+        val (constraintTraits, otherTraits) = this.allTraits.values
             .partition {
                 memberConstraintTraitsToOverride.contains(it.javaClass)
             }
 
         // No transformation required in case the given MemberShape has no constraints.
-        if (constrainedTraits.isEmpty())
+        if (constraintTraits.isEmpty())
             return null
 
-        // Create a new standalone shape of the same target as the target of the member e.g.
-        // someX : Centigrade, where integer : Centigrade, then the new shape should be
-        // integer: newShape
         val targetShape = model.expectShape(this.target)
+        if (targetShape !is ToSmithyBuilder<*>)
+            UNREACHABLE("member target shapes will always be buildable")
 
-        // The shape has to be a buildable type otherwise it cannot be refactored.
-        if (targetShape is ToSmithyBuilder<*>) {
-            when (val builder = targetShape.toBuilder()) {
-                is AbstractShapeBuilder<*, *> -> {
-                    // Use the target builder to create a new standalone shape that would
-                    // be added to the model later on. Keep all existing traits on the target
-                    // but replace the ones that the member shape overrides.
-                    val existingNonOverridenTraits =
-                        builder.allTraits.values.filter { existingTrait ->
-                            !constrainedTraits.any { it.toShapeId() == existingTrait.toShapeId() }
-                        }
+        when (val builder = targetShape.toBuilder()) {
+            is AbstractShapeBuilder<*, *> -> {
+                // Use the target builder to create a new standalone shape that would
+                // be added to the model later on. Keep all existing traits on the target
+                // but replace the ones that are overridden on the member shapes.
+                val existingNonOverriddenTraits =
+                    builder.allTraits.values.filter { existingTrait ->
+                        constraintTraits.none { it.toShapeId() == existingTrait.toShapeId() }
+                    }
 
-                    val newTraits = existingNonOverridenTraits + constrainedTraits + RefactoredStructureTrait(this.id)
+                val newTraits =
+                    existingNonOverriddenTraits + constraintTraits + SyntheticStructureFromConstrainedMemberTrait(
+                        this.id,
+                    )
 
-                    // Create a new standalone type that would be added to the model later on
-                    val shapeId = refactoredShapeId(model, existingRefactoredNames, this.id)
-                    val standaloneShape = builder.id(shapeId)
-                        .traits(newTraits)
-                        .build()
+                // Create a new standalone shape that will be added to the model later on
+                val shapeId = overriddenShapeId(model, additionalNames, this.id)
+                val standaloneShape = builder.id(shapeId)
+                    .traits(newTraits)
+                    .build()
 
-                    // Since the new shape has not been added to the model as yet, the current
-                    // memberShape's target cannot be changed to the new shape.
-                    // + OriginalShapeIdTrait(this.target)
-                    return MemberTransformation(standaloneShape, this, otherTraits)
-                }
-
-                else -> throw IllegalStateException("Constraint traits cannot to applied on ${this.id}") // FZ confirm how we are throwing exceptions
+                // Since the new shape has not been added to the model as yet, the current
+                // memberShape's target cannot be changed to the new shape.
+                return MemberShapeTransformation(standaloneShape, this, otherTraits)
             }
+
+            else -> throw IllegalStateException("Constraint traits cannot to applied on ${this.id}") // FZ confirm how we are throwing exceptions
         }
 
         throw IllegalStateException("Constraint traits can only be applied to buildable types. ${this.id} is not buildable") // FZ confirm how we are throwing exceptions
